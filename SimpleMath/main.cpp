d73f97f319fe43bac2826c5c962fa06adb6236bc
Mike Van Amburg <vanamburg.mv@gmail.com>
2019-11-25 18:07:09 -0600

    Initial Commit

---
+++ SimpleMath/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  SimpleMath
+//
+//  Created by Mike  Van Amburg on 11/25/19.
+//  Copyright © 2019 Mike Van Amburg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    double cart[6][10] = {
+        {1,2,3,4,5,6,7,8,9,10},
+        {12,13,14,15,16,17,18,19,20,21},
+    };
+    
+    
+    return 0;
+}
