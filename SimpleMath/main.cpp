63d7590ac0dfcc7ffc33cf43d8db0a21d300b723
Mike Van Amburg <vanamburg.mv@gmail.com>
2019-11-25 23:23:58 -0600

    version 1

--- SimpleMath/main.cpp
+++ SimpleMath/main.cpp
@@ -7,13 +7,51 @@
 //
 
 #include <iostream>
+#include <numeric>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-        bool repeat;
+    
+    //Varibles
+    double item;
+    double tax;
+    
+    //taxes are init
+    double taxClothingRate = 0.06;
+    double taxBeautyRate = 0.07;
+    double taxGroceryRate = 0.03;
+    double taxGardeningRate = 0.06;
+    double taxSchoolSuppliesRate = 0.03;
+    double taxTobaccoProductsRate = 0.1;
+    //taxes amount
+    double taxClothing;
+    double taxBeauty;
+    double taxGrocery;
+    double taxGardening;
+    double taxSchoolSupplies;
+    double taxTobaccoProducts;
+    
+    
+    
+    
+//    Arrays
+    //clothing array
+    double clothingItem[50] = {0.0}; //alocates memory
+    int clothingItems = 0;
+    double clothingSum = 0.00;
+    
+    //clothing array
+    double beautyItem[50] = {0.0}; //alocates memory
+    int beautyItems = 0;
+    double beautySum = 0.00;
+    
+    
+    
+    
+    bool repeat;
     do {
-        repeat = false;
         int itemCategory;
+//        Prompts the user for category and price of the product
         std::cout << "\n";
         std::cout << "****************************** \n";
         std::cout << "**  Item Category           ** \n";
@@ -23,38 +61,68 @@ int main(int argc, const char * argv[]) {
         std::cout << "**  4 For Gardening         ** \n";
         std::cout << "**  5 For School supplies   ** \n";
         std::cout << "**  6 For Tobacco products  ** \n";
+        std::cout << "**  0 For total  ** \n";
         std::cout << "****************************** \n\n";
 
         std::cout << "Enter the number for the items Category: ";
-        
         std::cin >> itemCategory;
-          //switch statement
+        
+        
+          //switch statement for users input.
           switch(itemCategory){
             case 0:
                   repeat = true;
+                  //0 is entered and program ends
             break;
             case 1:
-                  std::cout << "case 1";
+                  std::cout << "Enter the cost for the item: ";
+                  std::cin >> item;
+                  
+                  //takes users input of item cost and places it in array clothingItem
+                  clothingItem[clothingItems] = item;
+                  clothingItems++;
+
                   repeat = false;
             break;
             case 2:
-                  std::cout << "case 2";
+                  std::cout << "Enter the cost for the item: ";
+                  std::cin >> item;
+                  
+                  //takes users input of item cost and places it in array clothingItem
+                  beautyItem[beautyItems] = item;
+                  beautyItems++;
                   repeat = false;
-            break;
+                  break;
             case 3:
-                  std::cout << "case 3";
+                  std::cout << "Enter the cost for the item: ";
+                  std::cin >> item;
+                  tax = item * taxGroceryRate;
+                  std::cout << "your item plus taxes " << tax + item;
+                  std::cout << "taxes: " << tax;
                   repeat = false;
             break;
             case 4:
-                  std::cout << "case 4";
+                  std::cout << "Enter the cost for the item: ";
+                  std::cin >> item;
+                 tax = item * taxGardeningRate;
+                  std::cout << "your item plus taxes " << tax + item;
+                  std::cout << "taxes: " << tax;
                   repeat = false;
             break;
             case 5:
-                  std::cout << "case 5";
+                  std::cout << "Enter the cost for the item: ";
+                  std::cin >> item;
+                  tax = item * taxSchoolSuppliesRate;
+                  std::cout << "your item plus taxes " << tax + item;
+                  std::cout << "taxes: " << tax;
                   repeat = false;
             break;
             case 6:
-                  std::cout << "case 6";
+                  std::cout << "Enter the cost for the item: ";
+                  std::cin >> item;
+                  tax = item * taxTobaccoProductsRate;
+                  std::cout << "your item plus taxes " << tax + item;
+                  std::cout << "taxes: " << tax;
                   repeat = false;
             break;
             default:
@@ -62,6 +130,28 @@ int main(int argc, const char * argv[]) {
               repeat = false;
           }
     }
+        //ounce repeat is false the program will stop
         while(!repeat);
+        
+    //clothing
+        std::cout << "\n";
+        clothingSum = std::accumulate(clothingItem, clothingItem + clothingItems, clothingSum);
+       //tax is calculated at 6%
+        taxClothing = clothingSum * taxClothingRate;
+        
+    
+    //beauty
+         std::cout << "\n";
+         beautySum = std::accumulate(beautyItem, beautyItem + beautyItems, beautySum);
+        //tax is calculated at 6%
+         taxBeauty = beautySum * taxBeautyRate;
+         
+    
+    //sum of order.
+    std::cout << "Total Order: "<< clothingSum + beautySum << "\n";
+    std::cout << "Total Order: "<< taxClothing + taxBeauty << "\n";
+    
+    
+
         return 0;
 }
